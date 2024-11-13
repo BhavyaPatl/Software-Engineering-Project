682816ee60fb7b9579becb9be21f48001788488b
202201526-Elvis <202201526@daiict.ac.in>
2024-11-13 12:36:49 +0530

    Set up API call in Redux with thunk middleware

--- Codes/server/routes/route.js
+++ Codes/server/routes/route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import { userSignup , userLogin} from '../controller/user-controller.js';
+import { getProducts } from '../controller/product-controller.js';
 
 const router = express.Router();
 
@@ -8,6 +9,7 @@ const router = express.Router();
 router.post('/signup',userSignup);
 router.post('/login',userLogin);
 
+router.get('/products',getProducts);
 
 export default router;
  
