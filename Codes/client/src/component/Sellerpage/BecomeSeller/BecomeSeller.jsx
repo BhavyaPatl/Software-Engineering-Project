a2c87bd9691d7bbd38fa18c6071b265f6c0aaf15
202201526-Elvis <202201526@daiict.ac.in>
2024-11-18 23:11:17 +0530

    Frontend of Forgot Password for Seller

--- Codes/client/src/component/Sellerpage/BecomeSeller/BecomeSeller.jsx
+++ Codes/client/src/component/Sellerpage/BecomeSeller/BecomeSeller.jsx
@@ -1,4 +1,3 @@
-
 import { Dialog, Box, TextField, Typography, Button, styled ,IconButton, InputAdornment} from '@mui/material';
 import PasswordChecklist from 'react-password-checklist';
 import VisibilityIcon from '@mui/icons-material/Visibility';
@@ -7,7 +6,7 @@ import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import MuiPhoneNumber from 'material-ui-phone-number';
 import { useState,useContext } from "react";
 import logo from '../../home/header/logo.png'
-import { authenticatesellerSignup, authenticatesellerLogin } from "../../../service/api";
+import { authenticatesellerSignup, authenticatesellerLogin,authenticateForgotPasswordforSeller,authenticateVerifyOtpforSeller} from "../../../service/api";
 import { DataContext } from "../../../context/DataProvider";
 import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
@@ -87,6 +86,16 @@ const accountInitialValue = {
         view: 'signup',
         heading: "Hello new Seller",
         subHeading: 'Sign up with your mobile number to get started'
+    },
+    email: {
+        view: 'email',
+        heading: 'Forgot Password',
+        subHeading: 'Enter your email to receive the OTP'
+    },
+    otp: {
+        view: 'otp',
+        heading: 'Enter OTP',
+        subHeading: 'We have sent an OTP to your email and New Password'
     }
 };
 
@@ -104,6 +113,12 @@ const loginInitialValues = {
     password:''
 };
 
+const emailInitialValue = {
+    email: '',
+    otp: '',
+    password: ''
+}
+
 const BecomeSeller = ({open, setOpen}) => {
 
     const [account, toggleAccount] = useState(accountInitialValue.login);
@@ -113,6 +128,7 @@ const BecomeSeller = ({open, setOpen}) => {
     const [validemail, setvalidemail] = useState(false);
     const [showPassword, setShowPassword] = useState(false);
     const [passwordEntered, setPasswordEntered] = useState(false);
+    const [email, setEmail] = useState(emailInitialValue);
 
     const {setAccount} = useContext(DataContext);
 
@@ -124,6 +140,7 @@ const BecomeSeller = ({open, setOpen}) => {
         setvalidemail(false);
         setShowPassword(false);
         setPasswordEntered(false);
+        setEmail(emailInitialValue);
     };
 
     const handleKeyDown = (e) => {
@@ -160,6 +177,19 @@ const BecomeSeller = ({open, setOpen}) => {
         setSignup({ ...signup, [e.target.name]: e.target.value });
         console.log("Updated signup data:", signup);
     };
+
+    const onEmailChange = (e) => {
+        const { name, value } = e.target;
+
+        if (name === 'email' && !validateEmail(value)) {
+            setvalidemail('Please enter a valid email address');
+        } else {
+            setvalidemail(false); // Clear error if email is valid
+        }
+        
+        setEmail({ ...email, [e.target.name]: e.target.value });
+        console.log("Updated Email data:", email);
+    };
    
     const onValueChange = (e)=>{
         setLogin({...login,[e.target.name]: e.target.value});
@@ -167,10 +197,13 @@ const BecomeSeller = ({open, setOpen}) => {
 
     const loginUser = async () => {
         let response = await authenticatesellerLogin(login);
+        // console.log("Respose is",response);
+        const token =response.data.token;
+        console.log(token);
+        console.log(response);
         if (response.status === 200) {
-            // Cookies.set('token', response.data.token, {expires: 7});
             handleClose();
-            setAccount(response.data.user.firstname);
+            setAccount(response.data.seller.firstname);
         } else {
             setError(true);
         }
@@ -178,11 +211,31 @@ const BecomeSeller = ({open, setOpen}) => {
 
     const signupUser =async ()=>{
         let response =  await authenticatesellerSignup(signup);
-        console.log("response is" , response);
         if(!response) return;
-        // Cookies.set('token', response.data.token, {expires: 7});
         handleClose();
         setAccount(signup.firstname);
+    };
+
+    const handleSendOtp = async() => {
+        let response  = await authenticateForgotPasswordforSeller(email);
+        console.log(response);
+       if(!response){
+        alert('Please enter valid email address');
+       }
+       else{
+        toggleAccount(accountInitialValue.otp);
+       }
+    };
+    
+    const handleResetPassword = async() => {
+        let response = await authenticateVerifyOtpforSeller(email);
+        if(!response){
+            alert('Please enter valid otp');
+        }
+        else{
+            alert('Password reset successfully');
+            handleClose();
+        }  
     };
 
     const handleClickShowPassword = () => {
@@ -226,10 +279,10 @@ const BecomeSeller = ({open, setOpen}) => {
                             <Text>By continuing, you agree to DealsDone's Terms of Use and Privacy Policy.</Text>
                             <LoginButton onClick={() => loginUser()}>Login</LoginButton>
                             <Typography style={{ textAlign: "center" }}>OR</Typography>
-                            <RequestOTP>Request OTP</RequestOTP>
+                            <RequestOTP onClick={() => toggleAccount(accountInitialValue.email)}>Forgot Password</RequestOTP>
                             <CreateAccount onClick={toggleSignup} style={{marginTop: 'auto'}}>New to DealsDone? Let's create an account</CreateAccount>
                         </Wrapper>
-                    ) : (
+                    ) : account.view === 'signup' ? (
                         <Wrapper>
                             <TextField label="Enter Firstname" onChange={(e) => onInputChange(e)} name="firstname" variant="outlined" />
                             <TextField label="Enter Lastname" onChange={(e) => onInputChange(e)} name="lastname" variant="outlined" />
@@ -270,11 +323,43 @@ const BecomeSeller = ({open, setOpen}) => {
                            
                             <LoginButton onClick={() => signupUser()}>Continue</LoginButton>
                         </Wrapper>
+                    ) :  account.view === 'email' ? (
+                        <Wrapper>
+                            <TextField label="Enter Email" onChange={(e) => onEmailChange(e)} name="email" variant="outlined" />
+                            {validemail && <Error>{validemail}</Error>}
+                            <LoginButton onClick={() => handleSendOtp()}>Send OTP</LoginButton>
+                        </Wrapper>
+                    ) : (
+                        <Wrapper>
+                            <TextField label="Enter OTP" onChange={(e) => setEmail({...email,[e.target.name]: e.target.value })} name="otp" variant="outlined" />
+                            <TextField label="Enter New Password" onChange={(e) => setEmail({...email,[e.target.name]: e.target.value })} value = {email.password} name="password" variant="outlined"
+                                type={showPassword ? 'text' : 'password'}
+                                InputProps={{
+                                    endAdornment: (
+                                        <InputAdornment position="end">
+                                            <IconButton
+                                                onClick={handleClickShowPassword}
+                                                onMouseDown={handleMouseDownPassword}
+                                                edge="end"
+                                            >
+                                                {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                                            </IconButton>
+                                        </InputAdornment>
+                                    ),
+                                }}
+                            />
+                            <PasswordChecklist
+                                rules={["minLength", "specialChar", "number", "capital", "lowercase"]}
+                                minLength={8}
+                                value={email.password}
+                            />
+                            <LoginButton onClick={() => handleResetPassword()}>Reset Password</LoginButton>     
+                        </Wrapper>
                     )}
                 </Box>
             </Component>
         </Dialog>
-    )
+    );
 };
 
 export default BecomeSeller;
